135e0b2e89da6b17dbd12f72294622e0ec4b17ca
vtu29738 <vtu29738@veltech.edu.in>
2026-08-04 14:42:43 +0530

    Time: 0 ms (100%), Space: 43.5 MB (44.01%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public void sortColors(int[] nums) {
+        int start = 0;
+        int mid = 0;
+        int end = nums.length - 1;
+
+        while (mid <= end) {
+            switch (nums[mid]) {
+                case 0:
+                    swap(nums, start, mid);
+                    start++;
+                    mid++;
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+                case 2:
+                    swap(nums, mid, end);
+                    end--;
+                    break;
+            }
+        }
+    }
+
+    // Method to swap two elements
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums = {2, 0, 2, 1, 1, 0};
+
+        s.sortColors(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
