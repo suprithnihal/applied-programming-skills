162dc3081bdb841002e059d1b28c3ebf67567835
vtu29738 <vtu29738@veltech.edu.in>
2026-08-11 14:10:08 +0530

    Time: 0 ms (100%), Space: 46.5 MB (70.65%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Step 1: Detect if a cycle exists
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                // Step 2: Find the start of the cycle
+                ListNode entry = head;
+
+                while (entry != slow) {
+                    entry = entry.next;
+                    slow = slow.next;
+                }
+
+                return entry;
+            }
+        }
+
+        // No cycle
+        return null;
+    }
+}
