8b699389ad19e312dbf3a68dc68fb04eaf02e218
vtu29738 <vtu29738@veltech.edu.in>
2026-08-25 14:36:12 +0530

    Time: 35 ms (40.81%), Space: 102.7 MB (15.76%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class MinStack {
+
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
