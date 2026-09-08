93359ab3a47c31bef1e4512a8d9a16adda37671d
vtu29738 <vtu29738@veltech.edu.in>
2026-09-08 14:00:37 +0530

    Time: 0 ms (100%), Space: 42.9 MB (15.08%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,36 @@
+class MyQueue {
+
+    private Stack<Integer> in;
+    private Stack<Integer> out;
+
+    public MyQueue() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+
+    public void push(int x) {
+        in.push(x);
+    }
+
+    public int pop() {
+        moveIfNeeded();
+        return out.pop();
+    }
+
+    public int peek() {
+        moveIfNeeded();
+        return out.peek();
+    }
+
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+
+    private void moveIfNeeded() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+    }
+}
